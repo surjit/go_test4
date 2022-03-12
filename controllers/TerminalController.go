8a18b81c558f3d8e3e7fa8b29ab71c2b3aa86ab1
Surjit Sidhu <mail@surjitsidhu.com>
2022-03-11 22:47:26 -0700

    Revert "code moved to separate files"
    This reverts commit 42ea5e09b3b45399d7bb14097b56e0ab3db46fca.

--- controllers/TerminalController.go
+++ controllers/TerminalController.go
@@ -2,10 +2,18 @@ package controllers
 
 import (
 	"baljeet/cmd"
+	"fmt"
+	"github.com/creack/pty"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/websocket/v2"
+	"io"
+	"log"
 	"os"
 	"os/exec"
+	"os/user"
+	"strconv"
+	"strings"
+	"syscall"
 	"time"
 	"unicode/utf8"
 )
@@ -41,7 +49,147 @@ func TerminalHandler() fiber.Handler {
 	socket := websocket.New(func(ws *websocket.Conn) {
 		defer ws.Close()
 
-		Terminal(ws, ptmx, execCmd)
+		wsconn := &wsConn{
+			conn: ws,
+		}
+
+		ptmx = nil
+		execCmd = nil
+
+		availableShell := make(map[int]string)
+		availableShell[1] = "/bin/sh"
+		availableShell[2] = "/bin/bash"
+		availableShell[3] = "/usr/bin/bash"
+		availableShell[4] = "/bin/dash"
+		availableShell[5] = "/usr/bin/dash"
+		availableShell[6] = "/bin/rbash"
+		availableShell[7] = "/usr/bin/rbash"
+
+		osUser := "root"
+		Shell := "2"
+
+		shellIndex, err := strconv.Atoi(Shell)
+		if err != nil {
+			// handle error
+			_ = ws.WriteMessage(websocket.TextMessage, []byte(fmt.Sprintf("%s\r\n", "Error: Invalid request")))
+			return
+		}
+
+		sysUser, err0 := user.Lookup(osUser)
+
+		if err0 != nil {
+			_ = ws.WriteMessage(websocket.TextMessage, []byte(fmt.Sprintf("%s\r\n", "Error: User does not found")))
+			return
+		}
+
+		if osUser == "root" {
+			command = "/usr/bin/bash"
+		} else {
+			command = availableShell[shellIndex]
+		}
+
+		if ptmx == nil {
+			var msg Message
+			if err := ws.ReadJSON(&msg); err != nil {
+				log.Println("failed to decode message:", err)
+				return
+			}
+
+			rows, cols, err := cmd.WindowSize(msg.Data)
+			if err != nil {
+				if err = ws.WriteMessage(websocket.TextMessage, []byte(fmt.Sprintf("%s\r\n", err))); err != nil {
+					log.Println("write:", err)
+					return
+				}
+			}
+			winsize := &pty.Winsize{
+				Rows: rows,
+				Cols: cols,
+			}
+
+			c := cmd.Filter(strings.Split(command, " "))
+			if len(c) > 1 {
+				//nolint
+				execCmd = exec.Command(c[0], c[1:]...)
+			} else {
+				//nolint
+				execCmd = exec.Command(c[0])
+			}
+
+			Uid, _ := strconv.ParseUint(sysUser.Gid, 10, 32)
+			Gid, _ := strconv.ParseUint(sysUser.Gid, 10, 32)
+
+			execCmd.Dir = sysUser.HomeDir
+			execCmd.Env = append(os.Environ(), "USER="+osUser, "HOME="+sysUser.HomeDir)
+			execCmd.SysProcAttr = &syscall.SysProcAttr{}
+			execCmd.SysProcAttr.Credential = &syscall.Credential{Uid: uint32(Uid), Gid: uint32(Gid)}
+
+			ptmx, err = pty.StartWithSize(execCmd, winsize)
+			if err != nil {
+				log.Println("failed to create pty", err)
+				return
+			}
+		}
+
+		// write data to process
+		go func() {
+			for {
+				var msg Message
+				if err := ws.ReadJSON(&msg); err != nil {
+					log.Println("websocket closed")
+
+					if execCmd != nil {
+						_ = execCmd.Process.Kill()
+						_, _ = execCmd.Process.Wait()
+					}
+
+					// close current session, when browser terminal is closed
+					ptmx.Close()
+					ptmx = nil
+					execCmd = nil
+					return
+				}
+
+				if msg.Event == EventClose {
+					log.Println("close websocket")
+					ws.Close()
+					return
+				}
+
+				if msg.Event == EventResize {
+					rows, cols, err := cmd.WindowSize(msg.Data)
+					if err != nil {
+						log.Println(err)
+						return
+					}
+
+					winsize := &pty.Winsize{
+						Rows: rows,
+						Cols: cols,
+					}
+
+					if err := pty.Setsize(ptmx, winsize); err != nil {
+						log.Println("failed to set window size:", err)
+						return
+					}
+					continue
+				}
+
+				data, ok := msg.Data.(string)
+				if !ok {
+					log.Println("invalid message data:", data)
+					return
+				}
+
+				_, err := ptmx.WriteString(data)
+				if err != nil {
+					log.Println("failed to write data to ptmx:", err)
+					return
+				}
+			}
+		}()
+
+		_, _ = io.Copy(wsconn, ptmx)
 	})
 
 	// check process state
